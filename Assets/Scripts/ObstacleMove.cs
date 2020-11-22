97f7c74c71750403136cbe0c3c671fe991ec4461
Brian-MaherTU <71657390+Brian-MaherTU@users.noreply.github.com>
2020-11-22 14:31:54 +0000

    build6

---
+++ Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+
+    public float delta = 20.0f;
+    public float speed = 2.0f;
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.z += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
