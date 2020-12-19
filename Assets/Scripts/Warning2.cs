5222056a7a92ababb6bc15bf9c1aa57c4a16f05b
Brian-MaherTU <71657390+Brian-MaherTU@users.noreply.github.com>
2020-12-19 15:39:18 +0000

    Final

---
+++ Assets/Scripts/Warning2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warning2 : MonoBehaviour
+{
+    private AudioSource playSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
